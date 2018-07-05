95cd809885d2c984fb2fa38b0a3c44ae03d00b5b
德崇 吴 <214514867@qq.com>
2018-07-05 16:43:27 +0800

    增加rocks地图的怪物

--- makePetBin.py
+++ makePetBin.py
@@ -3,21 +3,17 @@ import os
 from pets import *
 
 
-skill1 = Skill("霸王拳", SkillType.DirectDamage, 10, 5)
-skill2 = Skill("霸王若水", SkillType.Heal, 10, 5)
-skill3 = Skill("霸王无敌轰", SkillType.AreaDamage, 7, 10)
-enermy_pet1 = Pet("秋田犬", 15, 5, skill1, 1, 0, 25, 13,  "resources\\images\\Pets\\pet00.png", 18)
-enermy_pet2 = Pet("皮尤", 25, 2, skill2, 1, 0, 20, 10,  "resources\\images\\Pets\\pet01.png", 18)
-enermy_pet3 = Pet("仙人兽", 20, 3, skill3, 1, 0, 30, 18,  "resources\\images\\Pets\\pet02.png", 18)
-enermy_pet4 = Pet("小飞天", 20, 15, skill1, 1, 0, 43, 24,  "resources\\images\\Pets\\pet03.png", 18)
-enermy_pet5 = Pet("冥伽", 20, 15, skill2, 1, 0, 500, 20,  "resources\\images\\Pets\\pet04.png", 18)
-enermy_pet6 = Pet("花纤纤", 50, 5, skill2, 1, 0, 31, 20,  "resources\\images\\Pets\\pet05.png", 18)
-enermy_pet7 = Pet("迪鲁", 30, 10, skill3, 1, 0, 50, 30,  "resources\\images\\Pets\\pet06.png", 18)
-pets = [enermy_pet1, enermy_pet2, enermy_pet3, enermy_pet4, enermy_pet5, enermy_pet6, enermy_pet7]
+skill1 = Skill("地狱拳", SkillType.DirectDamage, 50, 10)
+skill2 = Skill("地狱若水", SkillType.Heal, 100, 15)
+skill3 = Skill("地狱无敌轰", SkillType.AreaDamage, 30, 20)
+enermy_pet1 = Pet("冥伽", 150, 58, skill1, 1, 0, 250, 63,  "resources\\images\\Pets\\pet04.png", 18)
+enermy_pet2 = Pet("浮幽", 200, 40, skill2, 1, 0, 200, 78,  "resources\\images\\Pets\\pet07.png", 18)
+enermy_pet3 = Pet("奜", 100, 100, skill3, 1, 0, 300, 100,  "resources\\images\\Pets\\pet10.png", 18)
+pets = [enermy_pet1, enermy_pet2, enermy_pet3]
 
 for num, pet in enumerate(pets):
     pet_packet = pickle.dumps(pet)
-    with open(const.PETBIN_DIR + 'forest\\' + str(num) + '.bin', "wb") as object:
+    with open(const.PETBIN_DIR + 'rocks\\' + str(num) + '.bin', "wb") as object:
         object.write(pet_packet)
 
 print(os.listdir('D:/pyCharm/procedures/pets-master/resources/pet_bin/forest'))
